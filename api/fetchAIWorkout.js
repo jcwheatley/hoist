600ea07d4a9db54ee152bd0be12c248534c19aff
James Wheatley <jameswheatley@Jamess-MacBook-Pro.local>
2025-03-29 16:39:01 -0600

    slight tweaks for vercel config

--- api/fetchAIWorkout.js
+++ api/fetchAIWorkout.js
@@ -4,7 +4,7 @@ console.log("here!")
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export async function POST(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
